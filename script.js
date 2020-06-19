b70b8d3ed18bfae15177e0de9fdb9ffbf3bc122b
Rohmad <rohmad37@ymail.com>
2020-06-19 13:40:26 +0700

    tampilan edit malem

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-// Number.prototype.powerFive = function (n) {
-//   return Math.pow(this, 5);
-// };
+Number.prototype.powerFive = function (n) {
+  return Math.pow(this, 5);
+};
 
-// let angka = 2;
+let angka = 2;
 
-// console.log(angka.powerFive());
+console.log(angka.powerFive());
 
-let buttonValue = document.getElementById("btn-input");
+// let buttonValue = document.getElementById("btn-input");
 
-buttonValue.addEventListener("click", function () {
-  let inputValue = document.getElementById("input").value;
+// buttonValue.addEventListener("click", function () {
+//   let inputValue = document.getElementById("input").value;
 
-  Number.prototype.powerFive = function (inputValue) {
-    return Math.pow(this, 5);
-  };
+//   Number.prototype.powerFive = function (inputValue) {
+//     return Math.pow(this, 5);
+//   };
 
-  //   let angka = 2;
+//   //   let angka = 2;
 
-  console.log(inputValue.powerFive());
+//   console.log(inputValue.powerFive());
 
-  document.querySelector(
-    ".container .row .col-12 #result"
-  ).innerHTML = `${inputValue.powerFive()}`;
-});
+//   document.querySelector(
+//     ".container .row .col-12 #result"
+//   ).innerHTML = `${inputValue.powerFive()}`;
+// });
