5bce184b8c70bfc3680f0e9e71b2a575c5df0f7d
Rohmad <rohmad37@ymail.com>
2020-06-19 10:17:07 +0700

    Exercise Jumat 1

---
+++ script.js
@@ -0,0 +1,25 @@
+Number.prototype.powerFive = function (n) {
+  return Math.pow(this, 5);
+};
+
+let angka = 2;
+
+console.log(angka.powerFive());
+
+// let buttonValue = document.getElementById("btn-input");
+
+// buttonValue.addEventListener("click", function () {
+//   let inputValue = document.getElementById("input").value;
+
+//   Number.prototype.powerFive = function (n) {
+//     return Math.pow(this, 5);
+//   };
+
+//   let angka = 2;
+
+//   console.log(angka.powerFive());
+
+//   document.querySelector(
+//     ".container .row .col-12 #result"
+//   ).innerHTML = `${inputValue.powerFive()}`;
+// });
